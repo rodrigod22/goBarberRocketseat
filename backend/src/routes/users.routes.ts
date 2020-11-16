cba05501f1087faa7567aa7c345a7ab3b0ee84a9
rodrigo <rodrigogat5@hotmail.com>
2020-11-15 22:20:18 -0300

    upload avatar

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
 import CreateUserService from './../service/CreateUserService';
+import ensureAuthenticate from './../middlewares/ensureAuthenticate';
+import UpdateUserAvatarService from './../service/UpdateUserAvatarService';
+
 
 const usersRouter = Router();
 
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 
@@ -27,5 +36,27 @@ usersRouter.post('/', async (request, response) => {
   }
 
 });
+
+
+usersRouter.patch('/avatar', ensureAuthenticate, upload.single('avatar'),
+  async (request, response) => {
+
+    try {
+
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user)
+
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
+
+  });
 
 export default usersRouter;
